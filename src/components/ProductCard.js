4ea87599079ef9d1df0a20e2d0ff7c8ca865371b
ZJ <vfxprojectguy@gmail.com>
2023-11-02 00:12:49 -0700

    build special products section

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -23,7 +23,7 @@ const ProductCard = () => {
               <ReactStars
                 count={5}
                 size={24}
-                value="4.5"
+                value={4}
                 edit={false}
                 activeColor="#ffd700"
               />
